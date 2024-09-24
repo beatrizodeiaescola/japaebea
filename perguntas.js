8f957239e44173289f9655df8a61abad2f77e65b
beatrizodeiaescola <beatriz.cantao@escola.pr.gov.br>
2024-09-24 08:31:06 -0300

    cx

--- perguntas.js
+++ perguntas.js
@@ -2,98 +2,98 @@ criaCartao(
     'Biologia',
     'Quantos litros de sangue o corpo humano adulto tem?',
     '5 litros')
-    
-    criaCartao(
+
+criaCartao(
     'Biologia',
     'Como o esôfago atua?',
     'Através de movimentos peristálticos')
-    
-    criaCartao(
+
+criaCartao(
     'Biologia',
     'Do que o bile é constituído?',
     'Bicarbonato de sódio e sais biliares')
-    
-    criaCartao(
+
+criaCartao(
     'Biologia',
     'Quando o suco gástrico é produzido no estômago?',
     'Quando o alimento se faz presente no estômago')
-    
-    criaCartao(
+
+criaCartao(
     'Biologia',
     'Órgãos que formam o tubo digestório',
     'Boca, faringe, esôfago, estômago, intestino delgado e grosso.')
-    
-    criaCartao(
+
+criaCartao(
     'Filosofia',
     'A quem pertence a frase: “penso, logo existo”',
     'Decartes')
-    
-    criaCartao(
+
+criaCartao(
     'Geografia',
     'Qual o menor e o maior país e o maior país do mundo',
     'Maior: Russia e Menor: Vaticano')
-    
-    criaCartao(
+
+criaCartao(
     'Matemática',
     'Quantas casas decimais tem o numero pi?',
     'infinitas')
-    
-    criaCartao(
+
+criaCartao(
     'Química',
     'Atualmente, quantos elementos químicos existem na tabela periódica?',
     '118')
-    
-    criaCartao(
+
+criaCartao(
     'Física',
     'Quanto tempo a luz do sol leva para chegar a terra?',
     '8 minutos')
-    
-    criaCartao(
+
+criaCartao(
     'Geografia',
     'Qual a montanha mais alta do Brasil?',
     'Pico da neblina')
-    
-    criaCartao(
+
+criaCartao(
     ' Física',
     'Qual a velocidade da luz?',
     '299 792 458 m / s')
-    
-    criaCartao(
+
+criaCartao(
     ' Geografia',
     'Em que ano o primeiro homem a pisou na lua',
     '1969')
-    
-    criaCartao(
+
+criaCartao(
     'Biologia',
     'Qual tipo de sangue considerado doador universal?',
     'Tipo O')
-    
-    criaCartao(
+
+criaCartao(
     'Biologia',
     'Quais os cromossomos que determinam o sexo masculino?',
     'Y')
-    
-    criaCartao(
+
+criaCartao(
     'Biologia',
     'Como se chamam os vasos que transportam o sangue do coração para o resto do corpo?',
     'Artérias')
-    
-    criaCartao(
+
+criaCartao(
     'Geografia',
     'Em que séc. o continente europeu foi devastado pela peste bubônica?',
     'Séc. XIV')
-    
-    criaCartao(
-    'Matemática' ,
+
+criaCartao(
+    'Matemática',
     'Quantos graus são necessários para que dois ângulos sejam complementares?',
     '90°')
-    
-    criaCartao(
+
+criaCartao(
     'História',
     'Quem foi o primeiro presidente do Brasil?',
     'Deodoro da fonseca')
-    
-    criaCartao(
+
+criaCartao(
     'História',
     'Quantos presidentes a ditadura militar teve?',
     '5')
